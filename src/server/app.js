954c183138b3637c98c2d2aa4fb243f735def525
Til Gopfert <tilgoepfert@gmail.com>
2017-04-21 13:52:29 +0200

    restructure

--- src/server/app.js
+++ src/server/app.js
@@ -3,26 +3,17 @@
  */
 
 var express = require('express');
-var cons = require('consolidate');
 
 app = express();
 
-app.engine('html', cons.swig);
-
-app.set('view engine', 'html');
-app.set('views' , __dirname + '\\views');
-
-app.get('/', function(req, res) {
-  res.render('test.hbs', {
-    title: 'Root',
-    content: 'Hello World from Root'
-  })
+app.get('/', function (req, res) {
+  res.send('Hello World from root')
 });
 
 app.get('/test', function (req, res) {
   res.send('Hello World from test.')
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log('Now listening on localhost:3000')
 });
